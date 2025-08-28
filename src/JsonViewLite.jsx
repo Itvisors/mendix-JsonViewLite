45360f29fb0168aa6c83702d7a8b11ea494e9cca
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2025-08-28 08:36:17 +0200

    Show friendly error message for invalid JSON data

--- src/JsonViewLite.jsx
+++ src/JsonViewLite.jsx
@@ -9,7 +9,17 @@ export function JsonViewLite(props) {
     if (!jsonAttr.value) {
         return null;
     }
-    const jsonObject = JSON.parse(jsonAttr.value);
+    let jsonObject = null;
+    try {
+        jsonObject = JSON.parse(jsonAttr.value);
+    } catch (error) {
+        const message = props.name + " JSON data error: " + error.message;
+        return (
+            <div>
+                <span className="json-view-lite-widget-error">{message}</span>
+            </div>
+        );
+    }
     return (
         <JsonViewLiteComponent
             jsonData={jsonObject}
